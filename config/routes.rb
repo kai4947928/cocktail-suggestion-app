d1bd0a711c0c9cbad5bb54fce2e2c909bc78debb
Kaito Ono <kaito@example.com>
2024-12-17 12:17:48 +0900

    新規投稿画面(仮)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root "recipes#index"
-  resources :recipes, only: [:index, :show]
+  resources :recipes, only: [:index, :show, :new, :create]
 
   get '/auth/failure', to: redirect('/')
   delete '/logout', to: 'sessions#destroy'
